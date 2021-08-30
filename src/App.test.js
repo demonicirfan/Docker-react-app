5a1018211710cdba7ad6250630ffb98f95b0e4cf
demonicirfan <hashtag.irfan@gmail.com>
2021-08-30 22:12:14 +0530

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+it("renders learn react link", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it("renders learn react link", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
